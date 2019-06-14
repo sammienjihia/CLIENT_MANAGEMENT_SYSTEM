5f9f40f75af25b4e9ec649a334724f637b9a5aaa
sammienjihia <dilaninjihia@gmail.com>
2019-06-14 12:10:57 +0300

    added product_id to the fillable array

--- app/Lead.php
+++ app/Lead.php
@@ -7,4 +7,12 @@ use Illuminate\Database\Eloquent\Model;
 class Lead extends Model
 {
     //
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'description', 'source_id', 'client_contact_id', 'user_id', 'lead_code', 'product_id',
+    ];
 }
